b1f8d1e5dfe00fd7731f9f3f73f67170e9a86d04
unknown <Mouche@.(none)>
2011-05-10 17:59:03 +0200

    Added optionmanager.h/cpp, optionreader.h/cpp and option.cpp to the repo

---
+++ optionreader.cpp
@@ -0,0 +1,23 @@
+#include "optionreader.h"
+
+OptionReader::OptionReader()
+{
+
+}
+
+
+OptionReader::~OptionReader()
+{
+
+}
+
+const std::map<std::string, std::string>& OptionReader::values() const
+{
+    return m_values;
+}
+
+
+void OptionReader::readOptions()
+{
+    doReadOptions();
+}
