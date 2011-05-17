ef79a624a32783a133cc793edb20197fd781b80a
oKcerG <gr3cko@gmail.com>
2011-05-17 05:53:56 +0200

    Modified Schedule class to use std::function and to return std::unique_ptr instead of raw pointers

--- schedule.cpp
+++ schedule.cpp
@@ -8,9 +8,9 @@ Schedule::Schedule() : m_service(), m_work(m_service)
     boost::thread(boost::bind(&boost::asio::io_service::run, &m_service)); //run io_service in a thread and detach it
 }
 
-boost::asio::repeating_timer* Schedule::addTimeout(int seconds, timeout_handler handler)
+std::unique_ptr<boost::asio::repeating_timer> Schedule::addTimeout(int seconds, std::function<void (const boost::system::error_code&)> handler)
 {
-    boost::asio::repeating_timer* timer = new boost::asio::repeating_timer(m_service);
+    std::unique_ptr<boost::asio::repeating_timer> timer(new boost::asio::repeating_timer(m_service));
     timer->start(boost::posix_time::seconds(seconds), handler);
     return timer;
 }
--- schedule.h
+++ schedule.h
@@ -2,14 +2,14 @@
 #define SCHEDULE_H
 
 #include <boost/asio/io_service.hpp>
+//#include <memory>
 #include "basic_repeating_timer.h"
 
 class Schedule
 {
-    typedef void(*timeout_handler)(const boost::system::error_code&);
 public:
     Schedule();
-    boost::asio::repeating_timer* addTimeout(int seconds, timeout_handler handler); //give ownership of the timer to the caller, stops when deleted
+    std::unique_ptr<boost::asio::repeating_timer> addTimeout(int seconds, std::function<void (const boost::system::error_code&)> handler);
 
 private:
     boost::asio::io_service m_service;
