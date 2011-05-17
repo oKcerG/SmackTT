0ce82594eded7f70deb559ebb30a9c36eeebd335
oKcerG <gr3cko@gmail.com>
2011-05-17 05:53:56 +0200

    Modified Schedule class to use std::function and to return std::unique_ptr instead of raw pointers

--- main.cpp
+++ main.cpp
@@ -1,27 +1,36 @@
 #include "server.h"
 #include "schedule.h"
-#include <iostream>
+#include "windows.h"
 #include <boost/bind.hpp>
-#include <windows.h>
 
-void salut(const boost::system::error_code&)
+struct Foo
 {
-    std::cout << "salut" << std::endl;
-}
+    Foo (Schedule &schedule) : timer(schedule.addTimeout(1, boost::bind(&Foo::hello, this)))
+    {
+        std::cout << "created\n";
+    }
+    ~Foo()
+    {
+        std::cout << "destructed\n";
+    }
+    void hello()
+    {
+        std::cout << "hello" << std::endl;
+    }
+    std::unique_ptr<boost::asio::repeating_timer> timer;
+};
 
 int main()
 {
-    //Server server;
     Schedule schedule;
-    boost::asio::repeating_timer* timer = schedule.addTimeout(1, salut);
-    for (int i = 0;;++i)
     {
-        std::cout << "hello" << std::endl;
-        Sleep(2000);
-        if (i == 3)
-            delete timer;
+        Foo hi(schedule);
+        Sleep(10000);
+        std::cout << "release" << std::endl;
+        //hi.timer.release();
+        std::cout << "released" << std::endl;
     }
-
+        Sleep(5000);
     return 0;
 }
 
